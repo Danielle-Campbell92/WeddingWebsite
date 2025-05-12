ae7930987b7282fe022ad1b0eb4aafb702eb1e80
Danielle <dani.camp1125@gmail.com>
2025-05-12 14:31:51 -0400

    added background

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <h2>March 26, 2026</h2>
       <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVW0WYuNFvo_dzMGDlAR1WTXOgmAeoUAX1lA&s' alt='cartagena-photo'/>
       <p>Cartagena, Colombia, is a vibrant, historic city brimming with colonial charm, lively culture, and stunning Caribbean veiws. It's a city wher you can explore cobble-stone streets, relax on the beautiful beaches, enjoy delicious food, and experience the unique blend of history and modern energy that makes it so captivating.</p>
-      <p>On March 26, 2026, Danielle Campbell and Daniel Restrepo wish to pledge their love and commitment in front of friends and family.</p>
+      <p>On March 26, 2026, Danielle Campbell and Daniel Restrepo wish to pledge their love and commitment in front of friends and family. Explore our site for more information!</p>
     </div>
     </>
   )
