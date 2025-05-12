3a1e20053b1c59d2b6edcca0de60c060c164a705
Danielle <dani.camp1125@gmail.com>
2025-05-12 14:21:23 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div>
+      <h1>Danielle Campbell and Daniel Restrepo are getting married in Cartagena Colombia!</h1>
+      <h2>March 26, 2026</h2>
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVW0WYuNFvo_dzMGDlAR1WTXOgmAeoUAX1lA&s' alt='cartagena-photo'/>
+      <p>Cartagena, Colombia, is a vibrant, historic city brimming with colonial charm, lively culture, and stunning Caribbean veiws. It's a city wher you can explore cobble-stone streets, relax on the beautiful beaches, enjoy delicious food, and experience the unique blend of history and modern energy that makes it so captivating.</p>
+      <p>On March 26, 2026, Danielle Campbell and Daniel Restrepo wish to pledge their love and commitment in front of friends and family.</p>
+    </div>
+    </>
+  )
+}
+
+export default App
